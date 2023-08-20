ecf20b4c4101548665047188476ac7df1e9ae56e
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-08-20 18:11:25 +0200

    weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import { RandomCityService } from "./services/random-city";
+import { weatherApi } from "./api/weather-info";
 
 function App() {
-  function getRandomCityData() {
+  async function getRandomCityData() {
     const cityData = RandomCityService.getRandomCityData();
     console.log("City data:", cityData);
+    // console.log("City name:", cityData.name);
+    // console.log("City latitude:", cityData.latitude);
+    const weather = await weatherApi.getWeatherData(
+      cityData.latitude,
+      cityData.longitude
+    );
+    console.log("Weather data:", weather);
   }
 
   return (
---
+++ src/api/weather-info.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const lang = "fr";
+
+export class weatherApi {
+  static async getWeatherData(lat, lon) {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=${lang}&appid=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }`
+    );
+    return response.data;
+  }
+}
