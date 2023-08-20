53fe34a6c671fd4a9dfc67c86b41c6acf3044e71
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-08-20 19:54:57 +0200

    metric units

--- src/api/weather-info.js
+++ src/api/weather-info.js
@@ -7,7 +7,7 @@ export class weatherApi {
     const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=${lang}&appid=${
         import.meta.env.VITE_WEATHER_API_KEY
-      }`
+      }&units=metric`
     );
     return response.data;
   }
