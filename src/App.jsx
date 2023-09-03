7a65c2fe0850b7ac0336b6e05322ee8a1f8adc16
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-09-03 19:58:58 +0200

    correction bug loaderscreen au scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,51 @@ import { LoaderScreen } from "./components/LoaderScreen/LoaderScreen";
 import { Footer } from "./components/Footer/Footer";
 
 function App() {
-  async function getRandomCityData() {
-    const cityData = RandomCityService.getRandomCityData();
-    console.log("City data:", cityData);
-    const weather = await weatherApi.getWeatherData(
-      cityData.latitude,
-      cityData.longitude
-    );
-    console.log("Weather data:", weather);
+  async function getWeatherRandomCity() {
+    // const cityData = RandomCityService.getRandomCityData();
+    // console.log("City data:", cityData);
+    // const weather = await weatherApi.getWeatherData(
+    //   cityData.latitude,
+    //   cityData.longitude
+    // );
+    // console.log("Weather data:", weather);
+    console.log("getWeatherRandomCity");
   }
 
   const [showLoader, setShowLoader] = useState(false);
+  const [randomCityFetched, setRandomCityFetched] = useState(false);
+  const [scrollInProgress, setScrollInProgress] = useState(false);
 
-  const handleScroll = () => {
-    setShowLoader(true);
-    setTimeout(() => {
-      setShowLoader(false);
-    }, 5600); // 5.6s en millisecondes
+  const handleScroll = async () => {
+    if (!scrollInProgress) {
+      setScrollInProgress(true);
+      setShowLoader(true);
+
+      try {
+        await getWeatherRandomCity();
+        setRandomCityFetched(true);
+      } catch (error) {
+        console.error("Erreur lors de l'appel à getWeatherRandomCity", error);
+      } finally {
+        setTimeout(() => {
+          setShowLoader(false);
+          setScrollInProgress(false);
+        }, 5600); // Durée de l'animation de chargement
+      }
+    }
   };
 
   useEffect(() => {
+    getWeatherRandomCity();
+
     const handleInteraction = () => {
-      handleScroll();
+      if (!scrollInProgress && !showLoader) {
+        handleScroll();
+        console.log("handleInteraction");
+      }
     };
 
     window.addEventListener("wheel", handleInteraction);
-
-    // Ajoutez également l'écouteur pour les écrans tactiles
     window.addEventListener("touchmove", handleInteraction);
 
     return () => {
@@ -44,7 +62,9 @@ function App() {
   return (
     <div className={style.app}>
       <main className={style.main}>
-        <button onClick={getRandomCityData}>Obtenir une ville aléatoire</button>
+        <button onClick={getWeatherRandomCity}>
+          Obtenir une ville aléatoire
+        </button>
       </main>
       <Footer className={style.footer} />
       {showLoader && <LoaderScreen />}
